12cdbc10c2f5dc5c4a3b75ea93ce70b80c659c2c
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-08-03 12:59:31 -0400

    removed jqueryui and jquery mobile

--- Scripts/app.js
+++ Scripts/app.js
@@ -13,57 +13,10 @@
 (function () {
     "use strict";
 
-    function setActivePage() {
-        switch (document.title) {
-            case "Home":
-                document.getElementById("index").setAttribute("class", "active");
-                break;
-            case "About Me":
-                document.getElementById("about").setAttribute("class", "active");
-                break;
-            case "Projects":
-                document.getElementById("projects").setAttribute("class", "active");
-                break;
-            case "Contact Me":
-                document.getElementById("contact").setAttribute("class", "active");
-                break;
-        }
-    }
-
-
-
-    function loadNavBar() {
-        $("#mainNav").load("Partials/navbar.html", function () {
-            $("#projects").replaceWith("<li id='gallery'><a href='gallery.html'><i class='fa fa-picture-o fa-lg'></i> Gallery</a></li>");
-            setActivePage();
-        });
-    }
-
-    function loadFooter(){
-        $("#footer").load("Partials/footer.html",function(){
-            var year = document.getElementById("year");
-            var date = new Date();
-            year.innerText = date.getFullYear();
-        })
-    }
-
-    function readAddressBook(){
-        $.getJSON("Scripts/addressbook.json",
-        function(data){
-            var addressbook = data;
-            var contacts = addressbook.contacts;
-
-            $.each(contacts,function(contact){
-                console.log(contacts[contact]);
-            })
-        })
-    }
 
     // app entry function
     function init() {
-        loadNavBar();
-        readAddressBook();
-        loadFooter();
+
 
 
 
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,1 @@
 /// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/jquerymobile/index.d.ts" />
-/// <reference path="globals/jqueryui/index.d.ts" />
