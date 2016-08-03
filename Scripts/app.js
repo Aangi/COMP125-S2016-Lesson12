e9875d98f39bc8f61b904abeba30224691e4da9b
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-08-03 13:46:47 -0400

    added hellolabel to stage

--- Scripts/app.js
+++ Scripts/app.js
@@ -15,6 +15,7 @@
 
     var canvas;
     var stage;
+    var helloLabel;
 
     // app entry function
     function init() {
@@ -23,10 +24,22 @@
         stage = new createjs.Stage(canvas);
         createjs.Ticker.framerate = 60; // 60 frames per second
         createjs.Ticker.on("tick", gameLoop); // call gameLoop every frame
+        main();
     }
 
     function gameLoop() {
+        helloLabel.rotation += 5;
         stage.update(); // refresh the stage container
+    }
+
+
+    function main() {
+        helloLabel = new createjs.Text("Hello World!", "40px Consolas","#000000");
+        helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+        helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
+        helloLabel.x = 160;
+        helloLabel.y = 160;
+        stage.addChild(helloLabel);
     }
 
     // call init funciton when window finishes loading
