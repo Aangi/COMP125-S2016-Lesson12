8e284cfacf91b7649527a2bd62025b554e10188f
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-08-03 13:14:50 -0400

    added CreateJS suite to project

--- Scripts/app.js
+++ Scripts/app.js
@@ -13,13 +13,22 @@
 (function () {
     "use strict";
 
+    var canvas;
+    var stage;
 
     // app entry function
     function init() {
 
+        canvas = document.getElementById("canvas");
+        stage = createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60; // 60 frames per second
+        createjs.Ticker.on("tick", gameLoop); // call gameLoop every frame
 
 
+    }
 
+    function gameLoop() {
+        stage.update(); // refresh the stage container
     }
 
     // call init funciton when window finishes loading
